d7c1c453158f7489c3613ae863d4638714235613
Dallin Holder <dallinholder@Dallins-MacBook-Pro.local>
2015-07-11 06:48:59 +0800

    second rev of project

--- Post Card/ViewController.swift
+++ Post Card/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var undoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +30,28 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
-        // Adding a comment here to test committs
+        // Adding a comment here to test commits
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        enterNameTextField.hidden = true
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
     
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
+    @IBAction func undoButtonPressed(sender: UIButton) {
+        messageLabel.hidden = true
+        nameLabel.hidden = true
+        enterNameTextField.hidden = false
+        
+        mailButton.setTitle("Send Mail", forState: UIControlState.Normal)
+
+    }
+
 
 }
 
